de851b2016ffdeb01abe25a5fa17c05bbba163a7
Kevin Slote <kslote1@gmail.com>
2025-10-15 11:01:41 -0400

    Fixing black

--- causationentropy/graph/__init__.py
+++ causationentropy/graph/__init__.py
@@ -1,2 +1,6 @@
-from .utils import network_to_dataframe, pcmci_to_networkx, \
-    networkx_to_pcmci, pcmci_network_to_dataframe
+from .utils import (
+    network_to_dataframe,
+    pcmci_to_networkx,
+    networkx_to_pcmci,
+    pcmci_network_to_dataframe,
+)
